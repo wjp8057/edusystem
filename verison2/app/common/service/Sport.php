b54ff53a714277ce74f7114399ed0e16573e05e1
fangrenfu@126.com <comefirst0>
2017-03-22 11:13:08 +0800

    增加体测成绩查询（管理人员）

--- verison2/app/common/service/Sport.php
+++ verison2/app/common/service/Sport.php
@@ -17,14 +17,24 @@ namespace app\common\service;
 use app\common\access\MyService;
 
 class Sport extends MyService {
-    function getList($page=1,$rows=20,$studentno='%',$year=''){
+    function getList($page=1,$rows=20,$studentno='%',$year='',$classno='%',$school='',$score=''){
         $result=['total'=>0,'rows'=>[]];
         $condition=null;
-        if($studentno!='%') $condition['studentno']=array('like',$studentno);
-        if($year!='') $condition['year']=$year;
+        if($studentno!='%') $condition['sport.studentno']=array('like',$studentno);
+        if($year!='') $condition['sport.year']=$year;
+        if($classno!='%') $condition['students.classno']=array('like',$classno);
+        if($school!='') $condition['classes.school']=$school;
+        if($score!='') $condition['sport.score']=array('lt',$score);
         $data=$this->query->table('sport')->page($page,$rows)
-            ->field('recno,year,studentno,grade,score,date')->where($condition)->order('year')->select();
-        $count= $this->query->table('sport')->where($condition)->count();
+            ->join('students','students.studentno=sport.studentno')
+            ->join('classes','classes.classno=students.classno')
+            ->join('schools','schools.school=classes.school')
+            ->field('recno,sport.year,students.studentno,sport.grade,sport.score,sport.date,rtrim(classes.classname) classname,rtrim(students.name) studentname,
+            rtrim(schools.name) schoolname')->where($condition)->order('year,studentno')->select();
+        $count= $this->query->table('sport')
+            ->join('students','students.studentno=sport.studentno')
+            ->join('classes','classes.classno=students.classno')
+            ->where($condition)->count();
         if(is_array($data)&&count($data)>0)
             $result=array('total'=>$count,'rows'=>$data);
         return $result;
--- verison2/app/score/controller/Printer.php
+++ verison2/app/score/controller/Printer.php
@@ -10,6 +10,7 @@ namespace app\score\controller;
 use app\common\access\MyAccess;
 use app\common\access\MyController;
 use app\common\service\Score;
+use app\common\service\Sport;
 use app\common\service\Student;
 use app\common\service\ViewFinalScoreCourse;
 use app\common\vendor\PHPExcel;
@@ -63,5 +64,38 @@ class Printer extends MyController
             MyAccess::throwException($e->getCode(), $e->getMessage());
         }
         return json($result);
+    }
+
+    public function sport($page = 1, $rows = 20, $studentno = '%', $year = '', $classno= '%', $school = '',$score='')
+    {
+        $result = null;
+        try {
+            $obj = new Sport();
+            $result = $obj->getList($page, $rows,$studentno, $year, $classno, $school,$score);
+
+        } catch (\Exception $e) {
+            MyAccess::throwException($e->getCode(), $e->getMessage());
+        }
+        return json($result);
+    }
+
+    public function exportsport($studentno = '%', $year = '', $classno= '%', $school = '',$score=''){
+
+        $result=null;
+        try {
+            $obj = new Sport();
+            $result = $obj->getList(1, 10000,$studentno, $year, $classno, $school,$score);
+            $file="体质健康测试成绩";
+            $data=$result['rows'];
+            $sheet='全部';
+            $title=$file;
+            $template= array("year"=>"学年","studentno"=>"学号","studentname"=>"姓名","classname"=>"班级","schoolname"=>"学院",
+                "grade"=>"等级", "score"=>"成绩");
+            $string=array("studentno");
+            $array[]=array("sheet"=>$sheet,"title"=>$title,"template"=>$template,"data"=>$data,"string"=>$string);
+            PHPExcel::export2Excel($file,$array);
+        } catch (\Exception $e) {
+            MyAccess::throwException($e->getCode(), $e->getMessage());
+        }
     }
 }
